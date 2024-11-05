8243ae4c6f79daffa84beba7fa116bb4a8e87ccf
tracer-nju <tracer@nju.edu.cn>
2024-11-06 01:45:29 +0800

    Static hostname: KevinsDell        Icon name: computer-laptop          Chassis: laptop       Machine ID: 44f5712aeb0d4008bde474cc0c21c0ac          Boot ID: 3fa0d8e7864049d0ac11a0ef6e0c8771 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.8.0-40-generic     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Dell G15 5530  01:45:29 up  6:33,  1 user,  load average: 0.97, 0.76, 0.62

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -64,8 +64,8 @@ int asm_setjmp(asm_jmp_buf env) {
         "pop %%rbp;"
         : [a] "=m"(env[0]), [b] "=m"(env[1]), [c] "=m"(env[2]),
           [d] "=m"(env[3]), [e] "=m"(env[4]), [f] "=m"(env[5]),
-          [g] "=m"(env[6]), [h] "=m"(env[7])
-        :[tmp1] "=&r"(tmp1), [tmp2] "=&r"(tmp2)
+          [g] "=m"(env[6]), [h] "=m"(env[7])，[tmp1] "=&r"(tmp1), [tmp2] "=&r"(tmp2)
+        :
         : "memory","rbp"
     );
     return 0;
