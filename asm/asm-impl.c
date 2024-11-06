63cc05f092369f31c4f7266be3905a8f6b7eed27
tracer-nju <tracer@nju.edu.cn>
2024-11-06 12:09:58 +0800

    Static hostname: KevinsDell        Icon name: computer-laptop          Chassis: laptop       Machine ID: 44f5712aeb0d4008bde474cc0c21c0ac          Boot ID: c0f36a3aae2d42c9bdc530433cf1b42e Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.8.0-40-generic     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Dell G15 5530  12:09:58 up  1:43,  1 user,  load average: 0.82, 0.61, 0.55

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -42,17 +42,17 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
 }
 
 int asm_setjmp(asm_jmp_buf env) {
-    asm(
+    asm volatile(
         "push %%rbp;"
         "mov %%rsp, %%rbp;"
-        "movq (%%rbp), %%rax;"
-        "movq %%rax, %[a];"
         "movq %%rsp, %%rax;"
         "add $0x10, %%rax;"
+        "movq %%rax, %[a];"
+        "movq (%%rbp), %%rax;"
         "movq %%rax, %[b];"
-        "movq %%rbx, %[c];"
         "movq 0x8(%%rbp), %%rax;"
-        "movq %%rax, %[d];"
+        "movq %%rax, %[c];"
+        "movq %%rbx, %[d];"
         "movq %%r12, %[e];"
         "movq %%r13, %[f];"
         "movq %%r14, %[g];"
@@ -68,5 +68,21 @@ int asm_setjmp(asm_jmp_buf env) {
 }
 
 void asm_longjmp(asm_jmp_buf env, int val) {
+    asm volatile(
+    "movq [a], %%rsp;"
+    "movq [b], %%rbp;"
+    "movq [d], %%rbx;"
+    "movq [e], %%r12;"
+    "movq [f], %%r13;"
+    "movq [g], %%r14;"
+    "movq [h], %%r15;"
+    "movq [c], %%rax;"
+    "jmp *%%rax;"
+    :
+    :[a] "m" (env[0]), [b] "m" (env[1]), [c] "m" (env[2]),
+    [d] "m" (env[3]), [e] "m" (env[4]), [f] "m" (env[5]),
+    [g] "m" (env[6]), [h] "m" (env[7])
+    : "rbx", "r12", "r13", "r14", "r15", "rbp", "rsp", "rax"
+  );
   return;
 }
