611fb2b6bc0157a529863a47fa272b6e87ff7e81
tracer-nju <tracer@nju.edu.cn>
2024-11-06 15:21:04 +0800

    Static hostname: KevinsDell        Icon name: computer-laptop          Chassis: laptop       Machine ID: 44f5712aeb0d4008bde474cc0c21c0ac          Boot ID: c0f36a3aae2d42c9bdc530433cf1b42e Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.8.0-40-generic     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Dell G15 5530  15:21:04 up  4:54,  1 user,  load average: 0.47, 0.43, 0.39

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -32,7 +32,8 @@ asm (
 }
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
-  asm (
+//经过STFW，有专门针对内存复制的指令，这里直接使用
+asm (
 "rep movsb;"
 : "+D"(dest), "+S"(src), "+c"(n)
 :
@@ -43,6 +44,11 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
 
 int asm_setjmp(asm_jmp_buf env) {
     asm volatile(
+    //由于不太熟练，写的有点丑陋复杂，请助教老师读完我的注释再看代码
+    //使用默认存第一个参数的rdi存env，rax类似tmp变量
+
+    //待完善，如果用方括号形式让gcc分配8个寄存器代表env的8个元素，会出现段错误，set_jmp返回值无法修改
+    //初步怀疑gcc分配寄存器出现问题，导致某个或者某几个寄存器被重复使用
       "push %%rbp;"
       "mov %%rsp,%%rbp;"
       "movq 0x8(%%rbp),%%rax;" 
@@ -72,6 +78,12 @@ int asm_setjmp(asm_jmp_buf env) {
 }
 
 void asm_longjmp(asm_jmp_buf env, int val) {
+  //由于不太熟练，写的有点丑陋复杂，请助教老师读完我的注释再看代码
+  //使用默认存第一个参数的rdi存env，存第二个参数的rsi存val
+  //通过改rax的方式让set_jmp返回val值，改完rax之后rsi没用了，又用它存env里的pc值来跳转
+
+  //待完善，如果用方括号形式让gcc分配8个寄存器代表env的8个元素，会出现段错误，set_jmp返回值无法修改
+  //初步怀疑gcc分配寄存器出现问题，导致某个或者某几个寄存器被重复使用
   asm volatile(
     "movq %%rsi,%%rax;"
     "movq (%%rdi),%%rsi;"
