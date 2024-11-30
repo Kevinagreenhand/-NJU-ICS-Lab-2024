d0343b009de123486744cea885d44fbdac158e8e
tracer-nju <tracer@nju.edu.cn>
2024-11-30 12:22:02 +0800

    Static hostname: KevinsDell        Icon name: computer-laptop          Chassis: laptop       Machine ID: 44f5712aeb0d4008bde474cc0c21c0ac          Boot ID: b265d3ee24a648c7ba6ce20d9e765b11 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.8.0-40-generic     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Dell G15 5530  12:22:03 up  1:23,  1 user,  load average: 0.76, 0.52, 0.52

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -13,19 +13,19 @@ int *sieve(int n) {
   for (int i = 0; i <= n; i++)
     is_prime[i] = true;
 //更正确的方法
-for(int i=2;i*i<=n;i++){
-  if(is_prime[i]){
-    for(int j=i*i;j<=n;j+=i){
-      is_prime[j]=false;
-    }
-  }
-}    
-
-//  for (int i = 2; i <= n; i++) {
-//    for (int j = i + i; j <= n; j += i) {
-//      is_prime[j] = false;
+//for(int i=2;i*i<=n;i++){
+//  if(is_prime[i]){
+//    for(int j=i*i;j<=n;j+=i){
+//      is_prime[j]=false;
 //    }
 //  }
+//}    
+
+  for (int i = 2; i <= n; i++) {
+    for (int j = i + i; j <= n; j += i) {
+      is_prime[j] = false;
+    }
+  }
 
   int *p = primes;
   for (int i = 2; i <= n; i++)
