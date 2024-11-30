cb809289295e2eba925f6b1d6cc149a7eca7a631
tracer-nju <tracer@nju.edu.cn>
2024-11-30 20:08:00 +0800

    Static hostname: KevinsDell        Icon name: computer-laptop          Chassis: laptop       Machine ID: 44f5712aeb0d4008bde474cc0c21c0ac          Boot ID: b265d3ee24a648c7ba6ce20d9e765b11 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.8.0-40-generic     Architecture: x86-64  20:08:00 up  9:09,  1 user,  load average: 0.31, 0.27, 0.38

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -9,30 +9,20 @@ static bool is_prime[N];
 static int  primes[N];
 
 int *sieve(int n) {
-  int cnt=0;
-    memset(is_prime, true, sizeof(is_prime)); // 先全部标记为素数
-    is_prime[1] = false; // 1不是素数
-    for(int i = 2; i <= n; ++i) // i从2循环到n（外层循环）
+  int tmpnum=0;
+  is_prime[1] = true; 
+    for(int i = 2; i <= n; ++i) 
     {
-        if(is_prime[i]){ 
-          primes[cnt++] = i;}
-        // 如果i没有被前面的数筛掉，则i是素数
-        for(int j = 0; j <= cnt-1&& i * primes[j] <= n; ++j)
-        // 筛掉i的素数倍，即i的prime[j]倍
-        // j循环枚举现在已经筛出的素数（内层循环）
+        if(!is_prime[i]){ 
+          primes[tmp_num] = i;
+          tmp_num++;}
+        for(int j = 0; j <= tmp_num-1 && i * primes[j] <= n; ++j)
         {
-            is_prime[i * primes[j]] = false;
-            // 倍数标记为合数，也就是i用prime[j]把i * prime[j]筛掉了
-            if(i % primes[j] == 0) break;
-            // 最神奇的一句话，如果i整除prime[j]，退出循环
-            // 这样可以保证线性的时间复杂度
+            is_prime[i * primes[j]] = true;
+            if(i % primes[j] == 0) 
+              break;
         }
     }
-    primes[cnt]=0;
-//  for (int i = 2; i <= n; i++) {
-//    for (int j = i + i; j <= n; j += i) {
-//      is_prime[j] = false;
-//    }
-//  }
+  primes[cnt]=0;
   return primes;
 }
