aa70785df3ff7f6b4ea7a05dd61de1a251373dad
tracer-nju <tracer@nju.edu.cn>
2024-11-30 20:20:37 +0800

    Static hostname: KevinsDell        Icon name: computer-laptop          Chassis: laptop       Machine ID: 44f5712aeb0d4008bde474cc0c21c0ac          Boot ID: b265d3ee24a648c7ba6ce20d9e765b11 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.8.0-40-generic     Architecture: x86-64  20:20:37 up  9:22,  1 user,  load average: 0.62, 0.73, 0.60

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -9,20 +9,27 @@ static bool is_prime[N];
 static int  primes[N];
 
 int *sieve(int n) {
+  //使用了欧拉筛
   int tmpnum=0;
+  //这里我为了降低程序运行的时间，用了一个非常奇怪的操作。因为is_prime这个布尔数组
+  //的初始化是全为false的，而我们的筛法一开始要把整个数组都设为true，这样需要遍历
+  //一遍整个数组，会额外消耗时间。所以在这里我把所有应该是true的改成false，应该是
+  //false的改为true，之后把所有的条件表达式取反。这样就可以避免循环初始化浪费时间。
   is_prime[1] = true; 
   for(int i = 2; i <= n; ++i) 
+  {
+    if(!is_prime[i])
     {
-      if(!is_prime[i]){ 
-        primes[tmpnum] = i;
-        tmpnum++;}
-      for(int j = 0; j <= tmpnum-1 && i * primes[j] <= n; ++j)
-      {
-        is_prime[i * primes[j]] = true;
-        if(i % primes[j] == 0) 
-          break;
-      }
+      primes[tmpnum] = i;
+      tmpnum++;
     }
+    for(int j = 0; j <= tmpnum-1 && i * primes[j] <= n; j++)
+    {
+      is_prime[i * primes[j]] = true;
+      //if(i % primes[j] == 0) 
+      //  break;
+    }
+  }
   primes[tmpnum]=0;
   return primes;
 }
