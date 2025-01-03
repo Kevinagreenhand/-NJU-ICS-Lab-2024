09ddf402439f7d7e7218fd2e65734ed19aa481f8
231098216-Tian Bowen <231098216@smail.nju.edu.cn>
2025-01-03 19:24:02 +0800

    fixed a name mistake

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -9,7 +9,16 @@ static uint64_t cycle_cnt = 0;
 void cycle_increase(int n) { cycle_cnt += n; }
 
 // TODO: implement the following functions
+typedef struct {
+  bool validbit;
+  bool dirty;
+  uint64_t tag;
+  uint32_t data[BLOCK_SIZE>>2];
+} ACacheLine;
 
+ACacheLine* cache;
+static int associativity_size=0;
+static int group_num_width=0;
 uint32_t cache_read(uintptr_t addr) {
   return 0;
 }
@@ -18,7 +27,10 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
 }
 
 void init_cache(int total_size_width, int associativity_width) {
+  associativity_size = exp2(associativity_width);
+  group_nums = exp2(total_size_width - associativity_width - BLOCK_WIDTH);
 }
 
+//这个函数疑似不需要写，先空着
 void display_statistic(void) {
 }
