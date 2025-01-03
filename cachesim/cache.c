10259bcfa5250e5e70ecdde1721761d5a499bbac
tracer-nju <tracer@nju.edu.cn>
2025-01-03 23:47:44 +0800

    Static hostname: KevinsDell        Icon name: computer-laptop          Chassis: laptop       Machine ID: 44f5712aeb0d4008bde474cc0c21c0ac          Boot ID: 5fbced90edb24dfbb88d56e5d3989121 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.8.0-40-generic     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Dell G15 5530  23:47:44 up  8:02,  1 user,  load average: 1.57, 1.43, 1.26

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -10,21 +10,21 @@ void cycle_increase(int n) { cycle_cnt += n; }
 
 typedef struct 
 {
-  bool valid;
-  bool change;
+  bool validbit;
+  bool dirtybit;
   uint32_t tag;
   uint32_t data[BLOCK_SIZE>>2];
-}Cache;
+}ACacheLine;
 
-static Cache *cache;
+static ACacheLine *cachearr;
 static uint64_t associativity_size;
 static uint64_t group_nums_size=0;
 void write_back(uint32_t group_number,uint32_t lines){
-  cache[lines].valid=false;
-  if(cache[lines].change==1){
-  uintptr_t mem_addr=(cache[lines].tag<<group_nums_size)+group_number;
-  mem_write(mem_addr,(uint8_t*)(cache[lines].data));
-  cache[lines].change=0;
+  cachearr[lines].validbit=false;
+  if(cachearr[lines].dirtybit==1){
+  uintptr_t mem_addr=(cachearr[lines].tag<<group_nums_size)+group_number;
+  mem_write(mem_addr,(uint8_t*)(cachearr[lines].data));
+  cachearr[lines].change=0;
   }
 }
 
@@ -35,12 +35,12 @@ int line_choose(uintptr_t addr,uint32_t group_number,uint32_t tag){
     int end=(group_number+1)*every_group;
     assert(start>=0&&end>=0);
       for(int i=start;i<end;i++){
-      if(cache[i].valid==false){
+      if(cachearr[i].validbit==false){
         uintptr_t mem_addr=(addr>>BLOCK_WIDTH);
-        mem_read(mem_addr,(uint8_t*)(cache[i].data));
-        cache[i].valid=true;
-        cache[i].change=false;
-        cache[i].tag=tag;
+        mem_read(mem_addr,(uint8_t*)(cachearr[i].data));
+        cachearr[i].validbit=true;
+        cachearr[i].dirtybit=false;
+        cachearr[i].tag=tag;
         return i;
       }
   }
@@ -49,10 +49,10 @@ int line_choose(uintptr_t addr,uint32_t group_number,uint32_t tag){
     assert(replace<=end);
     write_back(group_number,replace);
     uintptr_t  mem_addr=(addr>>BLOCK_WIDTH);
-    mem_read(mem_addr,(uint8_t*)(cache[replace].data));
-    cache[replace].valid=true;
-    cache[replace].change=false;
-    cache[replace].tag=tag;
+    mem_read(mem_addr,(uint8_t*)(cachearr[replace].data));
+    cachearr[replace].validbit=true;
+    cachearr[replace].dirtybit=false;
+    cachearr[replace].tag=tag;
     return replace;
 }
 
@@ -61,7 +61,7 @@ int find(uint32_t group_number,uint32_t tag){
   int start=group_number*every_group;
   int end=(group_number+1)*(every_group);
   for(int i=start;i<end;i++){
-      if(cache[i].tag==tag&&cache[i].valid==1)  return i;
+      if(cachearr[i].tag==tag&&cachearr[i].validbit==true)  return i;
   }
   return -1;
 }
@@ -74,11 +74,11 @@ uint32_t cache_read(uintptr_t addr) {
   uint32_t group_addr=(addr&0x3f)>>2;
   int line_number=find(group_num,tag);
   if(line_number!=-1){
-      return cache[line_number].data[group_addr];
+      return cachearr[line_number].data[group_addr];
   }
   else{
     int line=line_choose(addr,group_num,tag);
-    return cache[line].data[group_addr];
+    return cachearr[line].data[group_addr];
   }
   return 0;
 }
@@ -93,20 +93,20 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
   int start=group_num*every_group;
   int end=(group_num+1)*(every_group);
   for(int i=start;i<end;i++){
-    if(cache[i].tag==tag){
+    if(cachearr[i].tag==tag){
       line_number=i;
       }
   }
   if(line_number!=-1){
-    cache[line_number].change=1;
-    cache[line_number].data[group_addr] &= (~wmask);
-	  cache[line_number].data[group_addr] |= (data & wmask);
+    cachearr[line_number].dirtybit=true;
+    cachearr[line_number].data[group_addr] &= (~wmask);
+	  cachearr[line_number].data[group_addr] |= (data & wmask);
   }
   else{
     int new_line=line_choose(addr,group_num,tag);
-    cache[new_line].change=1;
-    cache[new_line].data[group_addr] &= (~wmask);
-	  cache[new_line].data[group_addr] |= (data & wmask);
+    cachearr[new_line].dirtybit=true;
+    cachearr[new_line].data[group_addr] &= (~wmask);
+	  cachearr[new_line].data[group_addr] |= (data & wmask);
   }
 }
 
@@ -115,10 +115,10 @@ void init_cache(int total_size_width, int associativity_width) {
   associativity_size=exp2(associativity_width);
   assert(total_size_width > associativity_width);
   group_nums_size=total_size_width-BLOCK_WIDTH-associativity_width;
-  cache = (Cache*)malloc(sizeof(Cache) * exp2(total_size_width-BLOCK_WIDTH));
+  cachearr = (ACacheLine*)malloc(sizeof(ACacheLine) * exp2(total_size_width-BLOCK_WIDTH));
   for(int i=0;i<exp2(total_size_width-BLOCK_WIDTH);i++){
-    cache[i].valid=false;
-    cache[i].change=false;
+    cachearr[i].validbit=false;
+    cachearr[i].dirtybit=false;
   }
 }
 
