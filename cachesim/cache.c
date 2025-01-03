d874384a9c864840db31e2df64de8888c1c82520
tracer-nju <tracer@nju.edu.cn>
2025-01-03 21:54:19 +0800

    Static hostname: KevinsDell        Icon name: computer-laptop          Chassis: laptop       Machine ID: 44f5712aeb0d4008bde474cc0c21c0ac          Boot ID: 5fbced90edb24dfbb88d56e5d3989121 Operating System: Ubuntu 22.04.4 LTS           Kernel: Linux 6.8.0-40-generic     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Dell G15 5530  21:54:19 up  6:08,  1 user,  load average: 1.16, 1.06, 1.02

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -43,7 +43,6 @@ static uint64_t random_replace_a_line(uint64_t addr,uint64_t group_index,uint64_
 }
 uint32_t cache_read(uintptr_t addr) {
   uint64_t tag=addr>>(group_num_width+BLOCK_WIDTH);
-  uint64_t temp=(1<<group_num_width)-1;
   uint64_t group_index=(addr>>BLOCK_WIDTH)&((uint64_t)((1<<group_num_width)-1));
   uint64_t group_addr=(addr&0x3f)>>2;
   bool findhelp=false;
@@ -63,7 +62,6 @@ uint32_t cache_read(uintptr_t addr) {
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
   uint64_t tag=addr>>(group_num_width+BLOCK_WIDTH);
-  uint64_t temp=(1<<group_num_width)-1;
   uint64_t group_index=(addr>>BLOCK_WIDTH)&((uint64_t)((1<<group_num_width)-1));
   uint64_t group_addr=(addr&0x3f)>>2;
   bool findhelp=false;
@@ -71,8 +69,7 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
   for(int i=group_index*associativity_size;i<(group_index+1)*associativity_size;i++){
     if (cachearr[i].tag==tag){
       findrecord=i;
-      findhelp=true;
-      break;}
+      findhelp=true;}
   }
   if(findhelp==false){
     findrecord=random_replace_a_line(addr,group_index,tag);
