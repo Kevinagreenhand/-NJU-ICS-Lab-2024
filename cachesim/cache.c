289de32bcb334d6e37dd203e166acab0a61a98bc
231098216-Tian Bowen <231098216@smail.nju.edu.cn>
2025-01-03 19:33:34 +0800

    init_cache function finished

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -16,7 +16,7 @@ typedef struct {
   uint32_t data[BLOCK_SIZE>>2];
 } ACacheLine;
 
-ACacheLine* cache;
+ACacheLine* cachearr;
 static int associativity_size=0;
 static int group_num_width=0;
 uint32_t cache_read(uintptr_t addr) {
@@ -28,7 +28,17 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
 
 void init_cache(int total_size_width, int associativity_width) {
   associativity_size = exp2(associativity_width);
-  group_nums = exp2(total_size_width - associativity_width - BLOCK_WIDTH);
+  group_num_width = total_size_width - associativity_width-BLOCK_SIZE;
+  cachearr = (ACacheLine*)malloc(exp2(group_num_width)*associativity_size*sizeof(ACacheLine));
+  for(int i=0;i<exp2(group_num_width)*associativity_size;i++) {
+    //在把validbit设置为false的同时，进行了一定的初始化。
+    cachearr[i].validbit=false;
+    cachearr[i].dirty=false;
+    cachearr[i].tag=0;
+    for(int j=0;j<BLOCK_SIZE>>2;j++) {
+      cachearr[i].data[j]=0;
+    }
+  }
 }
 
 //这个函数疑似不需要写，先空着
